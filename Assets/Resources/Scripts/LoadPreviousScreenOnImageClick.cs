f31b1592d0f232d891a7b2c54ed4514b12b2c2c7
unknown <nagpalm7@gmail.com>
2020-11-13 11:37:46 +0530

    Add Script to Load Prev Screen - PrevScreenLoadScript

---
+++ Assets/Resources/Scripts/LoadPreviousScreenOnImageClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class LoadPreviousScreenOnImageClick : MonoBehaviour, IPointerClickHandler
+{
+	private int prevSceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevSceneToLoad = SceneManager.GetActiveScene().buildIndex - 1;
+    }
+
+    // Update is called once per frame
+    public void OnPointerClick(PointerEventData eventData){
+    	Debug.Log(prevSceneToLoad);
+    	if(prevSceneToLoad>=0){
+    		SceneManager.LoadScene(prevSceneToLoad);
+    	}
+    }
+}
